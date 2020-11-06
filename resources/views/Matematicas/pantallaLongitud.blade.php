258a398fe3b3b2ea1f0d3f06befab231c7908cbd
dmazzoni96 <72675950+dmazzoni96@users.noreply.github.com>
2020-11-05 20:36:51 -0600

    Sprint3 Daniela

--- resources/views/Matematicas/pantallaLongitud.blade.php
+++ resources/views/Matematicas/pantallaLongitud.blade.php
@@ -137,7 +137,7 @@
                 <img src="https://i.ibb.co/dkV8L43/largo-corto.jpg"  width="285" style="border-bottom-left-radius: 30% ">
                 <div class="card-body">
                     <h5 class="card-title" style="text-align: center">Unidades de Longitud</h5>
-                    <a href="http://learningkids.test/unidadLongitud" style=" background: #FF7800" href="" class="btn btn-primary">Leer</a>
+                    <a href="http://learningkidscodigo.test/unidadLongitud" style=" background: #FF7800" href="" class="btn btn-primary">Leer</a>
                 </div>
             </div>
         </div>
@@ -146,7 +146,7 @@
                 <img src="https://i.ibb.co/g6JY8p6/medidas.jpg"  width="285"  alt="..." style="border-bottom-left-radius: 5% ">
                 <div class="card-body">
                     <h5 class="card-title" style="text-align: center">Medir con las partes del cuerpo</h5>
-                    <a href="http://learningkids.test/unidadCorporal#" style=" background: #FF7800" class="btn btn-primary">Leer</a>
+                    <a href="http://learningkidscodigo.test/unidadCorporal#" style=" background: #FF7800" class="btn btn-primary">Leer</a>
                 </div>
             </div>
         </div>
