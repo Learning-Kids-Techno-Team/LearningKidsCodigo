6ede80ce22884cbe7211efc3f7e0066c68a992d9
Hector <lopezhector967@gmail.com>
2020-12-09 18:43:14 -0600

    Sprint 5 Hector.

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script> <i class="material-icons">favorite</i>
+            <p>¡Nunca te des por vencido, las grandes cosas llevan tiempo!</p>
+        </div>
+    </div>
+</footer>
