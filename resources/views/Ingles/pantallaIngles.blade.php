2e70c892e57be6c1bc7ee4bf199a05ec67624c2f
Hector <lopezhector967@gmail.com>
2021-02-05 17:52:11 -0600

    Sprint 6 Hector López

--- resources/views/Ingles/pantallaIngles.blade.php
+++ resources/views/Ingles/pantallaIngles.blade.php
@@ -50,25 +50,27 @@
         <div class="row mb-5 mt-5">
             <div class="col-md-12">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle  mx-auto d-block" />
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/44cpB8M/13410301-calendario-de-dibujos-animados-removebg-preview.png"  class="rounded-circle  bg-info img-thumbnail  mx-auto d-block"  width="150" />
                 <h4>Tiempo </h4>
                 </a>
             </div>
         </div>
+
 
             <div class="row mb-5">
                 <div class="col-md-3">
                 </div>
                 <div class="col-md-3">
                     <a href="">
-                    <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <img alt="Bootstrap Image Preview" src="https://i.ibb.co/Zhqx8pH/edificios-publicos-ciudad-23-2147522547-removebg-preview.png" class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
                     <h4>Lugares de la ciudad </h4>
 
                 </div>
                 <div class="col-md-3">
                     <a href="">
-                    <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <img alt="Bootstrap Image Preview" src="https://i.ibb.co/MZ7VFFN/conjunto-profesiones-personas-101903-1467-removebg-preview.png"  class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150"/>
                         <h4>Profesiones </h4>
+
                     </a>
                 </div>
                 <div class="col-md-3">
@@ -83,20 +85,23 @@
         <div class="row mb-5">
             <div class="col-md-4">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/84WqF6G/conjunto-coleccion-animales-dibujos-animados-29190-2951-removebg-preview.png"  class="rounded-circle mx-auto d-block bg-info img-thumbnail" width="150" />
                     <h4>Los animales </h4>
+
                 </a>
             </div>
             <div class="col-md-4">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/rwvYbCk/1649a406132586d9d8d9001cdf21f42d-removebg-preview.png" class="rounded-circle mx-auto d-block bg-info img-thumbnail " width="150" />
                     <h4>Medios de transporte </h4>
+
                 </a>
             </div>
             <div class="col-md-4">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/SyfBVNG/45092986-pequenas-crian-as-felizes-com-bloco-de-alfabeto-removebg-preview.png" class="rounded-circle mx-auto d-block bg-info img-thumbnail"  width="150" />
                     <h4>Abecedario</h4>
+
                 </a>
             </div>
         </div>
@@ -104,64 +109,74 @@
         <div class="row mb-5">
             <div class="col-md-3">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/SssjzCN/54086510-dibujado-a-mano-alzada-l-pices-de-colores-textura-de-dibujos-animados-removebg-preview.png" class="rounded-circle mx-auto d-block bg-info img-thumbnail" width="150" />
+                    <h4>Colores </h4>
+
+
                 </a>
             </div>
             <div class="col-md-3">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/1mDpDjv/ninos-dibujos-animados-123-numeros-97632-620-removebg-preview.png"  class="rounded-circle mx-auto d-block bg-info img-thumbnail" width="150" />
+                    <h4>Números </h4>
                 </a>
             </div>
             <div class="col-md-3">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/gvXfHG1/depositphotos-126531952-stock-illustration-family-members-happy-faces-flat-removebg-preview.png" class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Miembros de la familia </h4>
+
                 </a>
             </div>
             <div class="col-md-3">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/Tbn1H41/conjunto-iconos-colores-partes-cuerpo-humano-106317-22-removebg-preview.png"  class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Partes del cuerpo </h4>
+
                 </a>
             </div>
         </div>
         <div class="row mb-5">
             <div class="col-md-2">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview"src="https://i.ibb.co/cyCfQ3W/85414090-colecci-n-de-iconos-de-ropa-de-mujer-varias-prendas-de-vestir-para-mujeres-para-trabajar-ca.png" class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Prendas de vestir </h4>
+
                 </a>
             </div>
             <div class="col-md-2">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview"  src="https://i.ibb.co/8bMzXFT/86383784-conjunto-de-vector-de-tiles-escolares-fondo-de-regreso-a-la-escuela-con-papeler-a-removebg.png"  class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Útiles escolares </h4>
+
                 </a>
             </div>
             <div class="col-md-2">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/ysgM7Hh/fondo-frutas-verduras-diseno-plano-23-2148100345-removebg-preview-1.png"  class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Frutas y verduras </h4>
+
                 </a>
             </div>
             <div class="col-md-2">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/b63GL20/Captura-de-pantalla-2017-09-11-a-las-00-18-30-removebg-preview.png"  class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Emociones </h4>
+
                 </a>
             </div>
             <div class="col-md-2">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview" src="https://i.ibb.co/mC3JX3r/partes-casa-e1512554637817-removebg-preview.png"  class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Partes de la casa </h4>
+
                 </a>
             </div>
             <div class="col-md-2">
                 <a href="">
-                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
-                    <h4>Matemáticas</h4>
+                <img alt="Bootstrap Image Preview"  src="https://i.ibb.co/B3cwHzQ/formas-geometricas-de-acrilico-removebg-preview.png" class="rounded-circle mx-auto d-block  bg-info img-thumbnail" width="150" />
+                    <h4>Figuras geométricas </h4>
+
                 </a>
             </div>
         </div>
