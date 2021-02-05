cb05e3a9fad2858793e462cd80df3c6570a0fb02
Hector <lopezhector967@gmail.com>
2021-02-05 16:24:31 -0600

    Sprint 6 Hector López

---
+++ resources/views/Ingles/pantallaIngles.blade.php
@@ -0,0 +1,174 @@
+@extends('layouts.app')
+
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Tiempo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+
+
+
+    <!-- Seguimiento -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JWFFJETEMQ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-JWFFJETEMQ');
+    </script>
+
+
+
+
+    <style>
+        body {
+            background: #FFEECC;
+        }
+         h4{
+             text-align: center;
+             font-family: bold;
+         }
+
+
+
+
+
+
+
+
+    </style>
+</head>
+<body>
+@section('content')
+
+    <div class="container-fluid">
+        <div class="row mb-5 mt-5">
+            <div class="col-md-12">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle  mx-auto d-block" />
+                <h4>Tiempo </h4>
+                </a>
+            </div>
+        </div>
+
+            <div class="row mb-5">
+                <div class="col-md-3">
+                </div>
+                <div class="col-md-3">
+                    <a href="">
+                    <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Lugares de la ciudad </h4>
+
+                </div>
+                <div class="col-md-3">
+                    <a href="">
+                    <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                        <h4>Profesiones </h4>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                </div>
+        </div>
+        </div>
+
+
+
+
+    <div class="container-fluid">
+        <div class="row mb-5">
+            <div class="col-md-4">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Los animales </h4>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Medios de transporte </h4>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Abecedario</h4>
+                </a>
+            </div>
+        </div>
+    </div>
+        <div class="row mb-5">
+            <div class="col-md-3">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col-md-2">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-2">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-2">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-2">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-2">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+            <div class="col-md-2">
+                <a href="">
+                <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle mx-auto d-block" />
+                    <h4>Matemáticas</h4>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
+
+@endsection
+
