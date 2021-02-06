99128e8e3233eaedc244070f7db2fa228e2514dc
SindyGalindo29 <spame.29jm@gmail.com>
2021-02-05 21:04:32 -0600

    Sprint6 Sindy G.

--- resources/views/Ingles/pantallaIngles.blade.php
+++ resources/views/Ingles/pantallaIngles.blade.php
@@ -73,18 +73,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
     </style>
 </head>
 <body>
@@ -131,7 +119,6 @@
                         <img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.vedoque.com/blog/wp-content/uploads/2019/05/numeros-2Banimados.jpg">
                         <div class="carousel-caption">
                             <h4 class=" mx-auto d-block">
-                            <h4>
 
                             </h4>
                             <p>
@@ -173,7 +160,7 @@
     <div class="container-fluid">
         <div class="row mb-5 mt-5">
             <div class="col-md-12">
-                <a href="">
+                <a href="{{ url('/pTiempo') }}">
                 <img  style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/44cpB8M/13410301-calendario-de-dibujos-animados-removebg-preview.png"  class="rounded-circle   img-thumbnail  mx-auto d-block"  width="150" />
                 <h4>Tiempo </h4>
                 </a>
@@ -185,13 +172,13 @@
                 <div class="col-md-3">
                 </div>
                 <div class="col-md-3">
-                    <a href="">
+                    <a href="{{ url('/') }}">
                     <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/Zhqx8pH/edificios-publicos-ciudad-23-2147522547-removebg-preview.png" class="rounded-circle mx-auto d-block img-thumbnail" width="150" />
                     <h4>Lugares de la ciudad </h4>
-
+                    </a>
                 </div>
                 <div class="col-md-3">
-                    <a href="">
+                    <a href="{{ url('/') }}">
                     <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/MZ7VFFN/conjunto-profesiones-personas-101903-1467-removebg-preview.png"  class="rounded-circle mx-auto d-block   img-thumbnail" width="150"/>
                         <h4>Profesiones </h4>
 
@@ -208,7 +195,7 @@
     <div class="container-fluid">
         <div class="row mb-5">
             <div class="col-md-4">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/F8f8QxB/20754030-conjunto-de-animales-de-colecci-n-cabeza-de-dibujos-animados-removebg-preview.png"   class="rounded-circle mx-auto d-block  img-thumbnail" width="150" />
                     <h4>Los animales </h4>
 
@@ -222,7 +209,7 @@
                 </a>
             </div>
             <div class="col-md-4">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/SyfBVNG/45092986-pequenas-crian-as-felizes-com-bloco-de-alfabeto-removebg-preview.png" class="rounded-circle mx-auto d-block  img-thumbnail"  width="150" />
                     <h4>Abecedario</h4>
 
@@ -232,7 +219,7 @@
     </div>
         <div class="row mb-5">
             <div class="col-md-3">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/SssjzCN/54086510-dibujado-a-mano-alzada-l-pices-de-colores-textura-de-dibujos-animados-removebg-preview.png" class="rounded-circle mx-auto d-block img-thumbnail" width="150" />
                     <h4>Colores </h4>
 
@@ -240,20 +227,20 @@
                 </a>
             </div>
             <div class="col-md-3">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img  style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/1mDpDjv/ninos-dibujos-animados-123-numeros-97632-620-removebg-preview.png"  class="rounded-circle mx-auto d-block  img-thumbnail" width="150" />
                     <h4>Números </h4>
                 </a>
             </div>
             <div class="col-md-3">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/gvXfHG1/depositphotos-126531952-stock-illustration-family-members-happy-faces-flat-removebg-preview.png" class="rounded-circle mx-auto d-block   img-thumbnail" width="150" />
                     <h4>Miembros de la familia </h4>
 
                 </a>
             </div>
             <div class="col-md-3">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/Tbn1H41/conjunto-iconos-colores-partes-cuerpo-humano-106317-22-removebg-preview.png"  class="rounded-circle mx-auto d-block   img-thumbnail" width="150" />
                     <h4>Partes del cuerpo </h4>
 
@@ -262,42 +249,42 @@
         </div>
         <div class="row mb-5">
             <div class="col-md-2">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview"src="https://i.ibb.co/cyCfQ3W/85414090-colecci-n-de-iconos-de-ropa-de-mujer-varias-prendas-de-vestir-para-mujeres-para-trabajar-ca.png" class="rounded-circle mx-auto d-block   img-thumbnail" width="150" />
                     <h4>Prendas de vestir </h4>
 
                 </a>
             </div>
             <div class="col-md-2">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview"  src="https://i.ibb.co/8bMzXFT/86383784-conjunto-de-vector-de-tiles-escolares-fondo-de-regreso-a-la-escuela-con-papeler-a-removebg.png"  class="rounded-circle mx-auto d-block  img-thumbnail" width="150" />
                     <h4>Útiles escolares </h4>
 
                 </a>
             </div>
             <div class="col-md-2">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/ysgM7Hh/fondo-frutas-verduras-diseno-plano-23-2148100345-removebg-preview-1.png"  class="rounded-circle mx-auto d-block  img-thumbnail" width="150" />
                     <h4>Frutas y verduras </h4>
 
                 </a>
             </div>
             <div class="col-md-2">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/b63GL20/Captura-de-pantalla-2017-09-11-a-las-00-18-30-removebg-preview.png"  class="rounded-circle mx-auto d-block  img-thumbnail" width="150" />
                     <h4>Emociones </h4>
 
                 </a>
             </div>
             <div class="col-md-2">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview" src="https://i.ibb.co/mC3JX3r/partes-casa-e1512554637817-removebg-preview.png"  class="rounded-circle mx-auto d-block   img-thumbnail" width="150" />
                     <h4>Partes de la casa </h4>
 
                 </a>
             </div>
             <div class="col-md-2">
-                <a href="">
+                <a href="{{ url('/') }}">
                 <img style="background: aquamarine" alt="Bootstrap Image Preview"  src="https://i.ibb.co/B3cwHzQ/formas-geometricas-de-acrilico-removebg-preview.png" class="rounded-circle mx-auto d-block   img-thumbnail" width="150" />
                     <h4>Figuras geométricas </h4>
 
@@ -306,23 +293,26 @@
         </div>
     </div>
 
+
     <div class="container-fluid " id="banderin" >
         <div class="row " >
             <div class="col-md-12">
                 <img src="https://i.ibb.co/D7L6jV4/tren-removebg-preview.png"  height="150" >
             </div>
 
-
         </div>
-
-
     </div>
+
     <div id="separadorInferior2">
         <div class="content ">
 
         </div>
     </div>
 
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
 
