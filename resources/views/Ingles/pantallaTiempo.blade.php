7d7b738c3a4dc1b71ebc004cf79ad4f7a004622c
SindyGalindo29 <spame.29jm@gmail.com>
2021-02-06 00:55:40 -0600

    Sprint6 Sindy G.

--- resources/views/Ingles/pantallaTiempo.blade.php
+++ resources/views/Ingles/pantallaTiempo.blade.php
@@ -118,7 +118,7 @@
     <div class="container-fluid" id="dif">
         <div class="row" >
             <div class="col-md-12" id="fondo">
-                <img  class=" mx-auto d-block" src="https://i.ibb.co/j9CkdRN/comp-removebg-preview.png"/>
+                <img  class=" mx-auto d-block" src="https://i.ibb.co/VHr6JfL/fel-removebg-preview-1.png">
             </div>
         </div>
     </div>
@@ -128,12 +128,16 @@
     <div id="separadorDePagina" class="container-fluid">
         <div class="row">
             <div class="mx-auto d-block">
-                <img  width="340" height="250" class="mx-auto d-block" src="https://i.ibb.co/xYDY0yQ/lpo-removebg-preview.png">
-                <h2 class="text-center"><em>
+                <img  width="330" height="240" class="mx-auto d-block" src="https://i.ibb.co/xYDY0yQ/lpo-removebg-preview.png">
+                <h2 class="text-center"><em>El tiempo. (The time)
                     </em></h2>
 
                 <p class="text-center">
-                    <strong><em><br>   <br>
+                    <strong><em><br> El tiempo es una lección muy valiosa para la vida de los niños, especialmente si les hablamos sobre cómo transcurre.
+                            A medida que van creciendo, los niños aprenden cuánto tiempo es necesario para llevar a cabo una acción, por ejemplo:
+                            comenzar la escuela, ir de la casa al parque, la hora de la comida, o la hora de volver a casa.
+                            Por supuesto, todos estos eventos ocurren en pocas horas. Pero, si ayudamos a los peques a aprender los meses del año en inglés podrán comprender más
+                            fácilmente qué sucede con períodos más extendidos de tiempo como los días de la semana. <br>
                         </em></strong> </p>
             </div>
         </div>
