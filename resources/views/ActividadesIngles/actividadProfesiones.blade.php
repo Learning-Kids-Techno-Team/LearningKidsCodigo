c5a010877987b478dbb064d2b1be6fbde80a9882
LuzBonilla16 <luzalejandrabonilla@gmail.com>
2021-02-21 01:26:45 -0600

    Sprint 7 Luz Bonilla

--- resources/views/ActividadesIngles/actividadProfesiones.blade.php
+++ resources/views/ActividadesIngles/actividadProfesiones.blade.php
@@ -13,7 +13,7 @@
         gtag('config', 'G-JWFFJETEMQ');
     </script>
 
-    <title>Actividad Estaciones del año</title>
+    <title>Actividad Profesiones</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <style>
         .navbar{
