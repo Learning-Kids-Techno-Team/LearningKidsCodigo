27ab8b77de7a1d7cd840ab7cf954688b63bc194b
dmazzoni96 <72675950+dmazzoni96@users.noreply.github.com>
2021-03-07 13:06:03 -0600

    Sprint 8 Daniela Mazzoni

--- resources/views/Ingles/partes.blade.php
+++ resources/views/Ingles/partes.blade.php
@@ -185,7 +185,8 @@
                         <img   src="https://i.ibb.co/L0S2WK6/cw.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Cabeza<br>head</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1Un9y6znPRKBoKCv2gsnGa9gE5vubnYDx/preview" width="318" height="60"></iframe>
+
                     </div>
 
                 </div>
@@ -195,7 +196,8 @@
                         <img src="https://i.ibb.co/FDVyH9Z/ceja1.jpg" class="card__image">
                         <h1 class="card__name text-center"><strong>Ceja <br> eyebrown</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1KdG2Et3MDVXsy3PIH-SZVdn45tvW0sxx/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -204,7 +206,8 @@
                         <img src="https://i.ibb.co/JjdycPK/ojo.jpg" alt="ojo"  class="card__image">
                         <h1 class="card__name text-center"><strong>Ojo<br>eye</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1zJ6LHGRdQAMIGmd851N07EytADGgBMO4/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
 
@@ -227,7 +230,8 @@
                         <img src="https://i.ibb.co/fHLCfZB/nariz.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Nariz<br>nose</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/12LFLL43nDJ_siyNKkWmDcXn7TpmPzST8/preview" width="318" height="60"></iframe>
+
                     </div>
 
                 </div>
@@ -237,7 +241,8 @@
                         <img src="https://i.ibb.co/34cpczJ/boca.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Boca <br>mouth</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1GmtPZiEgWdxMOQ2I5P9rPByuD_xoN705/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -246,7 +251,8 @@
                         <img src="https://i.ibb.co/X4V5Nvj/pelo.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Pelo<br>hair</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/15sntSjuDKlLj6GBfhMDhGsGxYH78lS4P/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
 
@@ -302,7 +308,8 @@
                         <img src="https://i.ibb.co/Ws6cQQ1/cade.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Cintura<br>waist</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1iiP5g1BSchBFXSGgNs6ECq4XAfC3Ajd3/preview" width="318" height="60"></iframe>
+
                     </div>
 
                 </div>
@@ -312,7 +319,8 @@
                         <img  src="https://i.ibb.co/bjtKzHx/cos.jpg" class="card__image">
                         <h1 class="card__name text-center"><strong>Costilla<br>rib</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1wPrZXUvf6QK5WHusoOZnHP4od_rx37Z7/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -321,7 +329,8 @@
                         <img  src="https://i.ibb.co/ctrzgFc/espal.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Espalda<br>back</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1EZ2tXE0LmHYv5t2zwMYVgsh_I9Ywn0XO/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
 
@@ -344,7 +353,8 @@
                        <img src="https://i.ibb.co/zN3FXCN/omb.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Ombligo<br>navel </strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1nzXOv04DC5tULWz1_J_tea9e6yrkIz3n/preview" width="318" height="60"></iframe>
+
                     </div>
 
                 </div>
@@ -354,7 +364,7 @@
                         <img src="https://i.ibb.co/TPWJmxG/pecho.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Pecho<br>chest</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1cTumeTZEitIi2gxqzcMycsk5BA_i6d09/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -363,7 +373,7 @@
                         <img src="https://i.ibb.co/Q9Bz266/column.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Columna vertebral<br>spine</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1d_MmXprqRXKRoKBC3GmVyc_qxP24ZC1E/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
 
@@ -395,7 +405,7 @@
                         <img src="https://i.ibb.co/9VJFLrp/bra.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Brazo<br>arms</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1x2CwIyeaqMXulH3tn8WX_gBUXBwX2YxW/preview" width="318" height="60"></iframe>
                     </div>
 
                 </div>
@@ -405,7 +415,7 @@
                         <img src="https://i.ibb.co/vJRm44f/Illustration-of-greeting-with-elbow-to-elbow.jpg  " class="card__image">
                         <h1 class="card__name text-center"><strong>Codo<br>elbow</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1HW3NMqRMLM61NE-aaIs81JI2GVe5HHnf/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -414,7 +424,9 @@
                         <img src="https://i.ibb.co/7bV4zRr/dedo.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Dedo de la mano<br>finger</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+
+                        <iframe src="https://drive.google.com/file/d/1qncvIfGy5OMTpAKWUZQmiGHKkEGyBE61/preview" width="318" height="60"></iframe>
+
                     </div>
                 </div>
             </div>
@@ -443,7 +455,7 @@
                         <img src="https://i.ibb.co/ydWwbVP/pierrna.jpg"    class="card__image">
                         <h1 class="card__name text-center"><strong>Pierna<br>leg</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1_o8J8l-3KudPXjAFu0Ztzzx3U-LyQtq9/preview" width="318" height="60"></iframe>
                     </div>
 
                 </div>
@@ -453,7 +465,7 @@
                         <img src="https://i.ibb.co/R4Nv3R1/pieq.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Pie<br>foot</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1MLRmKhEQ2GZxXNs1WG1SRo1D8jMvPXiL/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -462,7 +474,7 @@
                         <img src="https://i.ibb.co/MCWghJM/pies.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Pies<br>feet</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1c3aBe4J76je3gaGkweYPbZ6zB6VBNUOo/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
 
@@ -485,7 +497,7 @@
                         <img src="https://i.ibb.co/jrzwWtZ/fem-t-r-p-en-fot.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Dedo del pie<br>toe</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1cOTRzhQ_6BjOs2CaeWzOyOhEM5D37pkH/preview" width="318" height="60"></iframe>
                     </div>
 
                 </div>
@@ -495,7 +507,7 @@
                         <img src="https://i.ibb.co/rGrKvxG/rod.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Rodilla<br>knee</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1f0bsMuSi7OEx1bjSKmqT3wjl3vGQ4kIG/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -504,7 +516,7 @@
                         <img src="https://i.ibb.co/0CH8k2F/tob.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Tobillo<br>ankle</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1AVkeRL6aDu7RSPOTuUWU3vmVD3QuIS4f/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
 
@@ -557,7 +569,7 @@
                         <img src="https://i.ibb.co/WkGwYSY/hue.jpg"    class="card__image">
                         <h1 class="card__name text-center"><strong>Hueso<br>bone</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1H9J6QIHU99m_f71LTylS6sgW3Qgk7_QG/preview" width="310" height="60"></iframe>
                     </div>
 
                 </div>
@@ -567,7 +579,7 @@
                         <img src="https://i.ibb.co/vVSR8xN/hig.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Higado<br>liver</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1rcw-KHKbZp0m4g0fWLJ6ZjabmRIYes3w/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -576,7 +588,7 @@
                         <img src="https://i.ibb.co/cDmjvP3/mus.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Músculo<br>muscle</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1fZiv9UaUY34zu2b5oKi8TdGcCvCT3U4X/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
 
@@ -599,7 +611,7 @@
                         <img src="https://i.ibb.co/dKw4fD7/pul.jpg"  class="card__image">
                         <h1 class="card__name text-center"><strong>Pulmón<br>lung</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1foF7rsHcZFvGFPfGJENUWjp3wV_PyKO_/preview" width="318" height="60"></iframe>
                     </div>
 
                 </div>
@@ -609,7 +621,7 @@
                         <img src="https://i.ibb.co/HzYXcKD/ven.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Vena<br>vein</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1eGfejBTh94lkZt3G83V_5Q_2dAzH0BLY/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
                 <!-- tarjetas3 -->
@@ -618,7 +630,7 @@
                         <img src="https://i.ibb.co/bH8SbMw/cor.jpg"   class="card__image">
                         <h1 class="card__name text-center"><strong>Corazón<br>heart</strong></h1>
                         <br>
-                        <iframe src="https://drive.google.com/file/d/1rAiGEMtD5teX3HVkGK4ppJSaYRfKqkhP/preview"  width="318" height="60"></iframe>
+                        <iframe src="https://drive.google.com/file/d/1bsFB0er-JJtOLi3UTm3FJrJjousQh2eA/preview" width="318" height="60"></iframe>
                     </div>
                 </div>
 
