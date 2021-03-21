a4783941380c9f82823563244433b2335101eade
SindyGalindo29 <spame.29jm@gmail.com>
2021-03-20 21:15:51 -0600

    Sprint 9 Sindy G.

--- resources/views/Ingles/frutas.blade.php
+++ resources/views/Ingles/frutas.blade.php
@@ -103,22 +103,24 @@
         }
 
         #tar1{
-            background: #CCEECC;
-            border:darkgreen 5px solid;
+            background: #fff6af;
+            border: yellow 5px dashed;
 
         }
 
         #tar2{
-            background: #f1DAFF;
-            border: fuchsia 5px solid;
+            background: #CCEECC;
+            border: #4caf50 5px dashed;
 
         }
 
         #tar3{
             background: #FFEECC;
-            border: darkorange 5px solid;
+            border: coral 5px dashed;
 
         }
+
+
 
     </style>
 
@@ -131,19 +133,25 @@
     <!-- contenedor text-->
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12" style="background: #fff6af ">
+            <div class="col-md-12" style="background: coral">
                 <div class="row ml-5 mr-5">
-                    <div class="col-md-7  mt-5" style="background: #DCFECB">
-                        <img class="mt-5 mx-auto d-block" >
+                    <div class="col-md-7  mt-5" style="background: #fff6af ">
+                        <img class="mx-auto d-block" src="https://i.ibb.co/6gs7h4D/cc-removebg-preview.png">
                     </div>
-                    <div class="col-xl-5  mt-5 " style=" background: lightyellow"  >
+                    <div class="col-xl-5  mt-5 " style=" background: lightyellow">
                         <h4  class="mt-5" style="color: #3B5686"><em>Inglés</em></h4>
                         <h3 class="mt-5" style="text-align: center">Frutas <em>(Fruits)</em></h3>
                         <p class="" id="pa">
+                            <br>Algunas son dulces, otras un poco más ácidas, pero la mayoría son perfectas para tomar un refrigerio saludable.
+                            Las frutas son unas de las maravillas de la naturaleza, hay mucha variedad y vienen en diferentes formas y colores,
+                            ¡así que es seguro que habrá una para cada gusto!
                             <br>
+                            <br>Debido a lo variadas que pueden ser las diferentes frutas tanto en colores y texturas como en sabores,
+                            se convierten en unos de los alimentos más divertidos para probar.
+                            <br>Aprender los nombres de las frutas puede ser muy divertido, aquí encontrarás una lista con los nombres de frutas
+                            en inglés y español para practicar con los niños.
                             <br>
-                            <br>
-                            <br>
+
 
                         </p>
                     </div>
@@ -153,38 +161,44 @@
     </div>
 
     <!-- contenedor de las tarjetas -->
-    <div class="container-fluid mt-5" >
+    <div class="container-fluid mt-5"><h2 class="mt-5"><strong><em>¡Nombre de frutas en inglés y español!</em></strong></h2><br>
         <div  class="row m-5" >
             <div class="col-md-4">
 
                 <!-- tarjeta 1 -->
-                <div id="tar1" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
-                    <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                <div id="tar1" class="card">
+                    <img class="mx-auto d-block" src="https://i.ibb.co/hf0mq73/uva1-removebg-preview.png" width="280" height="200" >
+                    <div class="card-body" >
+                        <h2 class="mt-5">
+                            Uvas: <strong>Grapes</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
             </div>
             <!-- tarjeta 2 -->
             <div class="col-md-4 ">
-                <div id="tar2" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar2" class="card" >
+                    <img class="mx-auto d-block"  src="https://i.ibb.co/sbTdsn5/cerez-removebg-preview.png" width="250" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Cereza: <strong>Cherry</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
             </div>
             <!-- tarjetas3 -->
             <div class="col-md-4">
-                <div id="tar3" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" src="https://i.ibb.co/2qzZKGJ/arand-removebg-preview.png" width="230" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Arándano: <strong>Blueberry</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
@@ -199,33 +213,39 @@
             <div class="col-md-4">
 
                 <!-- tarjeta 4 -->
-                <div id="tar1" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar1" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/y8PbhSS/lemon-removebg-preview.png" width="300" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Lima: <strong>Lime</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
             </div>
             <!-- tarjeta 5 -->
             <div class="col-md-4 ">
-                <div id="tar2" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar2" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/gZXj37n/limo1-removebg-preview.png" width="300" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Limón: <strong>Lemon</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
             </div>
             <!-- tarjeta 6 -->
             <div class="col-md-4">
-                <div id="tar3" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/1X4Gf7r/banana-removebg-preview.png" width="200" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Plátano: <strong>Banana</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
@@ -240,33 +260,274 @@
         <div  class="row m-5" >
             <div class="col-md-4">
                 <!-- tarjeta 7-->
-                <div id="tar1" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar1" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/rdhVjzg/manzana-removebg-preview.png" width="250" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Manzana: <strong>Apple</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
             </div>
             <!-- tarjeta 8 -->
             <div class="col-md-4 ">
-                <div id="tar2" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar2" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/L5mh44b/sandia1-removebg-preview.png" width="250" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
-                        <iframe  width="300" height="80"></iframe>
+                        <h2 class="mt-5">
+                            Sandia: <strong>Watermelon</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
             </div>
             <!-- tarjeta 9 -->
             <div class="col-md-4">
-                <div id="tar3" class="card" style="width: 18rem;">
-                    <img class="mx-auto d-block" id="im">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/GTCKJgK/melon-removebg-preview.png" width="250" height="200">
                     <div class="card-body">
-                        <h2 class="mt-5"></h2>
+                        <h2 class="mt-5">
+                            Melocotón: <strong>Peach</strong>
+                        </h2>
                         <iframe  width="300" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- contenedor de las tarjetas -->
+    <div class="container-fluid mt-5">
+        <div  class="row m-5" >
+            <div class="col-md-4">
+
+                <!-- tarjeta 1 -->
+                <div id="tar1" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/q56xP07/pi-a-removebg-preview.png" width="250" height="200" >
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Piña: <strong>Pineapple</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 2 -->
+            <div class="col-md-4 ">
+                <div id="tar2" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/p2skX8Y/fre1-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Fresa: <strong>Strawberry</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjetas3 -->
+            <div class="col-md-4">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/NyfnM8h/naran-removebg-preview.png" width="230" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                           Naranja: <strong>Orange</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- contenedor 2 -->
+    <div class="container-fluid mt-5" >
+        <div  class="row m-5" >
+            <div class="col-md-4">
+
+                <!-- tarjeta 4 -->
+                <div id="tar1" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/KWmzSKD/coco-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                           Coco: <strong>Coconut</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 5 -->
+            <div class="col-md-4 ">
+                <div id="tar2" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/1zx7tG0/per1-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Pera: <strong>Pear</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 6 -->
+            <div class="col-md-4">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/Vg9z4zy/aguacat-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Aguacate: <strong>Avocado</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- contenedor3 -->
+    <div class="container-fluid mt-5" >
+        <div  class="row m-5" >
+            <div class="col-md-4">
+                <!-- tarjeta 7-->
+                <div id="tar1" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/G2SfrZY/pom1-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Pomelo: <strong>Grapefruit</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 8 -->
+            <div class="col-md-4 ">
+                <div id="tar2" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/1fQg4RV/kiw1-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Kiwi: <strong>Kiwi</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 9 -->
+            <div class="col-md-4">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/QKykXVr/man-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Mango: <strong>Mango</strong>
+                        </h2>
+                        <iframe  width="300" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- contenedor de las tarjetas -->
+    <div class="container-fluid mt-5">
+        <div  class="row m-5" >
+            <div class="col-md-4">
+
+                <!-- tarjeta 1 -->
+                <div id="tar1" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/Zzpv1Gc/higo-removebg-preview.png" width="300" height="200" >
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Higo: <strong>Fig</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 2 -->
+            <div class="col-md-4 ">
+                <div id="tar2" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/PGzk1FK/ciruela-removebg-preview.png" width="300" height="200" >
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Ciruela: <strong>Plum</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjetas3 -->
+            <div class="col-md-4">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/DQyPrVJ/mandarina-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Mandarina: <strong>Tangerine</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container-fluid mt-5">
+        <div  class="row m-5" >
+            <div class="col-md-4">
+
+                <!-- tarjeta 1 -->
+                <div id="tar1" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/LxMQHzG/maracu-removebg-preview.png" width="200" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Maracuyá: <strong>Passion fruit</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjeta 2 -->
+            <div class="col-md-4 ">
+                <div id="tar2" class="card" >
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/d4vwL9m/granada-removebg-preview.png" width="280" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                             Granada: <strong>Pomegranate</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <!-- tarjetas3 -->
+            <div class="col-md-4">
+                <div id="tar3" class="card">
+                    <img class="mx-auto d-block" id="im" src="https://i.ibb.co/J2WxQMh/fram-removebg-preview.png" width="250" height="200">
+                    <div class="card-body">
+                        <h2 class="mt-5">
+                            Frambuesa: <strong>Raspberry</strong>
+                        </h2>
+                        <iframe  width="230" height="80"></iframe>
 
                     </div>
                 </div>
