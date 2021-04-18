1391a6382926cea5ad1c49a4deb08cedd7671539
SindyGalindo29 <spame.29jm@gmail.com>
2021-04-17 22:06:33 -0600

    Sprint 11 Sindy G.

--- resources/views/ActividadesTecnologia/pantallaActividadTecno.blade.php
+++ resources/views/ActividadesTecnologia/pantallaActividadTecno.blade.php
@@ -177,7 +177,7 @@
                             <img src="https://i.ibb.co/xF9st8C/wi.jpg" class="card mx-auto d-block" height="180" width="200">
                             <div class="card-body">
                                 <h5 class="card-title" align="center">Actividad Ventana de windows</h5>
-                                <a href=" {{ url('/') }}" class="btn">Ver</a>
+                                <a href=" {{ url('/actividadWindows') }}" class="btn">Ver</a>
                             </div>
                         </div>
                     </div>
