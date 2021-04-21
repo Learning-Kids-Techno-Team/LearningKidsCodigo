f7d01ff55ff4981760d46a247793325c6b5f7fe2
LuzBonilla16 <luzalejandrabonilla@gmail.com>
2021-04-20 22:05:46 -0600

    Sprint 11 Luz Bonilla

--- resources/views/Tecnologia/barraDeTareasWindows.blade.php
+++ resources/views/Tecnologia/barraDeTareasWindows.blade.php
@@ -167,9 +167,10 @@
 
                 </h2>
                 <p>
-                    Identificado con el logo de windows, nos da acceso al menú de inicio, quien nos brinda acceso a todas la opciones del sistema operativo.
+                    Identificado con el logo de windows,  nos da acceso al menú de inicio, quien nos brinda acceso a todas la opciones del sistema operativo.
 
                 </p>
+
             </div>
             <div class="col-md-6">
                 <img class="mx-auto d-block"  src="https://i.ibb.co/xSxXxGS/inicio-removebg-preview.png"  width="200" >
