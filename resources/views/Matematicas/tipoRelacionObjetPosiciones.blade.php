f7b3d76146d77be4214bf9e63a052a5cdf93c162
dmazzoni96 <72675950+dmazzoni96@users.noreply.github.com>
2021-07-12 22:46:14 -0600

    Sprint 15 Daniela Mazzoni

--- resources/views/Matematicas/tipoRelacionObjetPosiciones.blade.php
+++ resources/views/Matematicas/tipoRelacionObjetPosiciones.blade.php
@@ -47,7 +47,7 @@
 
         #separadorInferior .content{
 
-            width: 110%;
+            width: 100%;
             height: 100px;
             margin-left: -5px;
             background: #FF7800;
@@ -65,11 +65,6 @@
 
 
 
-        #color{
-            color: darkgreen;
-
-        }
-
     </style>
 </head>
 <body>
@@ -77,66 +72,76 @@
 @section('content')
 
 <!-- contenedor del gif-->
-<div class="container-fluid" id="h">
+<div class="container-fluid" >
     <div class="row ">
-        <div class="col-md-4">
+        <div class="col-xl-4 p-2">
             <img class="mx-auto d-block" src="https://i.ibb.co/SrpcJKJ/dib-removebg-preview-1.png" width="350" />
 
         </div>
-        <div class="col-md-8">
-            <h2 id="color"><em><br>¡RELACIÓN DE OBJETOS!</em></h2>
-            <h3 id="color"><em><br>Reconozco posiciones.</em></h3><br>
-            <p id="color"><em><strong>
+        <div class="col-xl-8 p-2">
+            <h2 class="text-center"> <br>¡RELACIÓN DE OBJETOS!</h2>
+            <h3 class="text-center" > Reconozco posiciones.</h3>
+            <p class="text-justify"><strong>
                         El significado de posición vincula a la postura corporal (de pie, hacia la derecha, izquierda, sentado, etc.)...
                         Posición viene del latín positio, que significa ubicación.
-                    </strong></em></p><br><br>
+                    </strong></p><br><br>
             <img class="mx-auto d-block"  width="100" />
         </div>
     </div>
 </div>
 
 <!-- contenedor video-->
-<div class="container-fluid" id="color"><h2><em><br></em></h2>
+<div class="container-fluid" >
     <div class="row">
-        <div class="col-md-12" >
-            <iframe id="tar2" class="mx-auto d-block" src="https://drive.google.com/file/d/1XQ_JQEh0f-LLLNQE6bkvR7YjHTj3tGad/preview" width="650" height="400"></iframe>
+        <div class="col-xl-12 p-2" >
+            <iframe id="tar2" class="mx-auto d-block" src="https://drive.google.com/file/d/1XQ_JQEh0f-LLLNQE6bkvR7YjHTj3tGad/preview"
+                    width="400" ></iframe>
             <h5 align="center"></h5>
         </div>
     </div>
 </div>
 
 <!-- contenedor de Imagenes-->
-<div class="container-fluid  center-block pl-5" id="color"><h2><em><br>Observo y aprendo: </em></h2>
-    <div class="row m-5" >
-        <div class="col-md-6">
-            <div class="card"  style="width: 28rem;">
-                <img class="mx-auto d-block" src="https://i.ibb.co/0m4yVQL/images-2-removebg-preview.png" width="200" height="300">
+<div class="container-fluid" ><br><h2 class="text-center">Observo y aprendo: </h2>
+    <h2 class="text-center">¡Posiciones!</h2>
+    <div class="row " >
+        <div class="col-xl-6 p-2">
+            <div class="card mx-auto d-block"  style="width: 20rem;">
+                <br>
+                <img class="mx-auto d-block" src="https://i.ibb.co/0m4yVQL/images-2-removebg-preview.png"
+                     width="200">
                 <h5 class="card-title" align="center">Izquierda</h5>
             </div>
         </div>
-        <div class="col-md-6">
-            <h2 id="color"><em><br>¡Posiciones!</em></h2>
-            <div class="card" style="width: 28rem;">
-                <img class="mx-auto d-block" src="https://i.ibb.co/WfHyJwP/nin-removebg-preview.png" width="200" height="300">
+
+        <div class="col-xl-6 p-2">
+            <div class="card mx-auto d-block" style="width: 20rem;">
+                <br>
+                <img class="mx-auto d-block" src="https://i.ibb.co/WfHyJwP/nin-removebg-preview.png"
+                     width="167" >
                 <h5 class="card-title" align="center">Derecha</h5>
             </div>
         </div>
     </div>
 </div>
 <!-- contenedor -->
-<div class="container-fluid  center-block pl-5" id="color"><h2><em><br>¡Repito!</em></h2>
-    <div class="card">
-        <div class="row m-5" >
-            <div class="col-md-4">
-                <img class="mx-auto d-block" src="https://i.ibb.co/ygc9x4z/mjj-removebg-preview.png" width="300"/>
+<div class="container-fluid" ><h2 class="text-center"><br>¡Repito!</h2>
+    <div class="card mx-auto d-block">
+        <div class="row " >
+            <div class="col-xl-4 p-2">
+                <img class="mx-auto d-block" src="https://i.ibb.co/ygc9x4z/mjj-removebg-preview.png"
+                     width="200"/>
                 <h5 class="card-title" align="center">Izquierda</h5>
             </div>
-            <div class="col-md-4">
-                <img class="mx-auto d-block" src="https://i.ibb.co/crthqGt/ppk-removebg-preview.png" width="300"/>
+            <div class="col-xl-4 p-2">
+                <br>
+                <img class="mx-auto d-block" src="https://i.ibb.co/crthqGt/ppk-removebg-preview.png"
+                     width="200"/>
                 <h5 class="card-title" align="center">El niño tiene helados en sus manos: Derecha e Izquierda</h5>
             </div>
-            <div class="col-md-4">
-                <img class="mx-auto d-block" src="https://i.ibb.co/mC6285T/s511-removebg-preview.png" width="320"/>
+            <div class="col-xl-4 p-2">
+                <img class="mx-auto d-block" src="https://i.ibb.co/mC6285T/s511-removebg-preview.png"
+                     width="200"/>
                 <h5 class="card-title" align="center">Derecha</h5>
             </div>
         </div>
@@ -144,19 +149,22 @@
     </div>
 </div>
 <!-- contenedor -->
-<div class="container-fluid  center-block pl-5" id="color"><h2><em><br>¡Analizo y aprendo!</em></h2>
-    <div class="card">
-        <div class="row m-5" >
-            <div class="col-md-4">
-                <img class="mx-auto d-block" src="https://i.ibb.co/0MYVcvB/ilus-removebg-preview-1.png" width="300"/>
+<div class="container-fluid  " ><h2 class="text-center"><br>¡Analizo y aprendo!</h2>
+    <div class="card mx-auto d-block">
+        <div class="row" >
+            <div class="col-xl-4 p-2">
+                <img class="mx-auto d-block" src="https://i.ibb.co/0MYVcvB/ilus-removebg-preview-1.png"
+                     width="200"/>
                 <h5 class="card-title" align="center"></h5>
             </div>
-            <div class="col-md-4">
-                <img class="mx-auto d-block" src="https://i.ibb.co/3RxVJvC/derecha-removebg-preview.png" width="300"/>
+            <div class="col-xl-4 p-2">
+                <img class="mx-auto d-block" src="https://i.ibb.co/3RxVJvC/derecha-removebg-preview.png"
+                     width="200"/>
                 <h5 class="card-title" align="center"></h5>
             </div>
-            <div class="col-md-4">
-                <img class="mx-auto d-block" src="https://i.ibb.co/YZxxg9c/ilustrac-removebg-preview.png" width="320"/>
+            <div class="col-xl-4 p-2">
+                <img class="mx-auto d-block" src="https://i.ibb.co/YZxxg9c/ilustrac-removebg-preview.png"
+                     width="200"/>
                 <h5 class="card-title" align="center"></h5>
             </div>
         </div>
@@ -164,22 +172,29 @@
     </div>
 </div>
 
-<div class="container-fluid  center-block pl-5" id="color"><h2><em><br>¡Respondo en voz alta!</em></h2>
-    <div class="row m-5">
-        <div class="col-md-3">
-            <img  class="mx-auto d-block" src="https://i.ibb.co/v150nc4/4871-removebg-preview.png" width="220" height="200">
+<div class="container-fluid  " ><h2 class="text-center"><br>¡Respondo en voz alta!</h2>
+    <div class="row ">
+        <div class="col-xl-3 p-2">
+            <br>
+            <img  class="mx-auto d-block" src="https://i.ibb.co/v150nc4/4871-removebg-preview.png"
+                  width="214">
             <h5 class="card-title" align="center">¿En que lado tiene la niña su oso?</h5>
         </div>
-        <div class="col-md-3">
-            <img  class="mx-auto d-block" src="https://i.ibb.co/myY6LCc/sss-removebg-preview.png" width="220" height="200">
+        <div class="col-xl-3 p-2">
+            <br>
+            <img  class="mx-auto d-block" src="https://i.ibb.co/myY6LCc/sss-removebg-preview.png"
+                  width="220" >
             <h5 class="card-title" align="center">¿Que lado señala el niño?</h5>
         </div>
-        <div class="col-md-3">
-            <img  class="mx-auto d-block" src="https://i.ibb.co/RyZcYqf/ayhk-removebg-preview.png" width="220" height="200">
+        <div class="col-xl-3 p-2">
+            <img  class="mx-auto d-block" src="https://i.ibb.co/RyZcYqf/ayhk-removebg-preview.png"
+                  width="220" >
             <h5 class="card-title" align="center">¿En que lado sostiene la niña su estrella?</h5>
         </div>
-        <div class="col-md-3">
-            <img  class="mx-auto d-block" src="https://i.ibb.co/jk29TJ4/Circo-removebg-preview.png" width="220" height="200">
+        <div class="col-xl-3 p-2">
+            <br><br>
+            <img  class="mx-auto d-block" src="https://i.ibb.co/jk29TJ4/Circo-removebg-preview.png"
+                  width="249">
             <h5 class="card-title" align="center">¿Que sostiene el payasito con su mano izquierda?</h5>
         </div>
         <div class="card-body">
@@ -189,28 +204,38 @@
 </div>
 
 <!-- contenedor -->
-<div class="container-fluid center-block pl-5" id="color"><h2><em><br>Las posiciones izquierda y derecha:</em></h2>
-    <div class="row m-5">
-        <div class="col-md-4">
-            <div class="card" style="width: 20rem;">
-                <img class="mx-auto d-block" src="https://i.ibb.co/1MgsHWJ/hh.jpg" width="310" height="230">
+<div class="container-fluid ">
+
+    <div class="row ">
+        <div class="col-xl-4 p-2">
+
+            <h2 class="text-center" >¡Izquierda  y derecha!</h2>
+            <div class="card mx-auto d-block" style="width: 15rem;">
+                <img class=" mx-auto d-block" src="https://i.ibb.co/1MgsHWJ/hh.jpg" width="228"
+                >
             </div>
         </div>
-        <div class="col-md-4">
-            <h2 id="color"><em><br></em>¡En direcciones!</h2>
-            <div class="card" style="width: 20rem;">
-                <img class="mx-auto d-block" src="https://i.ibb.co/9VLzwhQ/IZQ-DER.jpg" width="310" height="230">
+
+        <div class="col-xl-4 p-2">
+
+            <h2 class="text-center" >¡En direcciones!</h2>
+            <div class="card mx-auto d-block" style="width: 15rem;">
+                <img class="mx-auto d-block" src="https://i.ibb.co/9VLzwhQ/IZQ-DER.jpg" width="228"
+
+                    >
             </div>
         </div>
-        <div class="col-md-4"><h2 id="color"><em><br></em>¿Izquierda o derecha?</h2>
-            <div class="card" style="width: 20rem;">
-                <img class="mx-auto d-block" src="https://i.ibb.co/drYGBkJ/unnamed-1.jpg" width="310" height="230">
+        <div class="col-xl-4 p-2">
+            <h2 class="text-center">¡Posiciones!</h2>
+            <div class="card mx-auto d-block" style="width: 15rem;">
+                <img class="mx-auto d-block" src="https://i.ibb.co/drYGBkJ/unnamed-1.jpg"
+                     width="228" >
             </div>
         </div>
     </div>
 </div>
 
-
+<br><br>
 <!-- separador -->
 <div id="separadorInferior">
     <div class="content">
