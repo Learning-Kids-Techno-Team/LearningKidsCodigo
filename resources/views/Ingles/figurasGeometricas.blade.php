eb198db0e44ddea1566c63a6b6d88320f4b8db06
dmazzoni96 <72675950+dmazzoni96@users.noreply.github.com>
2021-08-07 21:23:51 -0600

    Sprint 16 Daniela Mazzoni

--- resources/views/Ingles/figurasGeometricas.blade.php
+++ resources/views/Ingles/figurasGeometricas.blade.php
@@ -47,31 +47,7 @@
             background: aquamarine;
         }
 
-        h1{
-            color: #0b3251;
-            font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-        }
 
-
-
-
-        h4{
-            text-decoration-color: #3B5686;
-            text-align: center;
-        }
-
-        h2{
-            text-align: center;
-            color: #0b3251;
-            font-family: bold;
-            background-image: url("https://i.ibb.co/FVpRGSC/text-background-plate-5a3bff7ca8e220-5994791815138814686918-removebg-preview.png");
-        }
-
-        h3{
-            text-align: center;
-            color: #123c24 ;
-            font-family: bold;
-        }
 
         #bor{
             border: oldlace 5px dashed;
@@ -85,11 +61,7 @@
         .navbar{
             background: aquamarine;
         }
-        h5{
-            font-family: bold;
-            padding-top: 20px;
-            text-align: center;
-        }
+
 
         #bord{
             background:#DCFECB;
@@ -110,16 +82,18 @@
     <!-- contenedor text-->
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12" style="background: #fff6af">
+            <div class="col-xl-12" style="background: #fff6af">
                 <div class="row ml-5 mr-5">
                     <div class="col-xl-5  mt-5 " style=" background: #DCFECB" >
-                        <h3  class="mt-5" style="color: #3B5686"><em>Inglés</em></h3>
-                        <h1 class="mt-5" style="text-align: center">Formas y Figuras Geométricas en inglés<br></h1>
+                        <br><br>
+                        <h3  class="text-center" ><em>Inglés</em></h3>
+                        <h1 class="text-center" >Formas y Figuras Geométricas en inglés<br></h1>
                         <p class="" id="pa">
                         </p>
                     </div>
-                    <div class="col-md-7  mt-5" style="background: #DCFECB">
-                        <img class="mt-5 mx-auto d-block" src="https://i.ibb.co/7Qdkznp/area-y-perimetro-figuras-geometricas-removebg-preview.png" >
+                    <div class="col-xl-7  mt-5" style="background: #DCFECB">
+                        <img class="mt-2 mx-auto d-block" src="https://i.ibb.co/7Qdkznp/area-y-perimetro-figuras-geometricas-removebg-preview.png"
+                        width="300">
 
                     </div>
                 </div>
@@ -130,12 +104,13 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-xl-12 p-2">
                 <br><br><br>
                 <h5>
                     Al aprender las figuras geométricas en inglés, adquieres un variado conjunto de habilidades tales como la comunicación verbal, que resulta útil cuando practican las descripciones de las cosas que pueden ver, bases para lectura y escritura ya que obtienen lo fundamental para la identificación de las letras y números, bases de geometría que te servirán en el futuro proceso de aprendizaje de matemáticas, y creatividad. Todas estas habilidades mejorarán significativamente tus experiencias de vida, permitiéndoles interactuar de una mejor manera con el mundo,
                     estableciendo conexiones útiles entre los objetos y otorgándoles una mejor apreciación del arte.
                 </h5>
+                <br><br>
                 <h1 style="text-align: center">!Comencemos!</h1>
                 <br><br><br>
             </div>
@@ -144,32 +119,44 @@
 
 
     <div class="container-fluid">
-        <div class="row mb-5">
-            <div class="col-md-3">
+        <div class="row ">
+            <div class="col-xl-3 p-2">
                 <img class="mx-auto d-block"   src="https://i.ibb.co/3BNMwnZ/ef4548d1309854ac3bb1ad6b582a4b38-removebg-preview.png"  width="180" />
+              <br>
+               <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/1.mp3" controls >
                 </audio>
                 <h2>Circle – círculo</h2>
+               </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-xl-3 p-2">
                 <img  class="mx-auto d-block"   src="https://i.ibb.co/1QSkMZN/istockphoto-1208851179-170667a-removebg-preview.png" alt="istockphoto-1208851179-170667a-removebg-preview"    width="220"/>
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/2.mp3" controls >
                 </audio>
                 <h2>Diamond – diamante</h2>
+                </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-xl-3 p-2">
 
                 <img  class="mx-auto d-block" src="https://i.ibb.co/9ty9vLZ/vector-plano-del-dise-o-del-amor-del-emoticon-del-coraz-n-del-personaje-de-dibujos-animados-del-kawa.png"    width="168" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/3.mp3" controls >
                 </audio>
                 <h2>Heart – corazón</h2>
-
+                </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-xl-3 p-2">
+
                 <img  class="mx-auto d-block"  src="https://i.ibb.co/f2BBjMB/png-clipart-hexagon-cartoon-drawing-hexagon-shape-smiley-polygon-removebg-preview.png"    width="200" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/4.mp3" controls >
                 </audio>
                 <h2>Hexagon - hexágono</h2>
+                </div>
             </div>
         </div>
     </div>
@@ -177,33 +164,46 @@
 
 
     <div class="container-fluid">
-        <div class="row mb-5">
-            <div class="col-md-3">
+        <div class="row ">
+            <div class="col-xl-3 p-2">
+                <br>
                 <img class="mx-auto d-block" src="https://i.ibb.co/wKZ2rQ5/13f2f7d681f293147cd203c43ff8b230-removebg-preview.png"   width="185" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/5.mp3" controls >
                 </audio>
                 <h2>Rectangle – rectángulo</h2>
+                </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-xl-3 p-2">
+               <br>
                 <img  class="mx-auto d-block"   src="https://i.ibb.co/2738FZL/png-transparent-geometric-shape-disk-rectangle-geometry-emoticon-square-angle-text-triangle-removebg.png"     width="145"/>
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/6.mp3" controls >
                 </audio>
                 <h2>Square – cuadrado</h2>
+                </div>
             </div>
-            <div class="col-md-3">
-
+            <div class="col-xl-3 p-2">
+<br>
                 <img  class="mx-auto d-block" src="https://i.ibb.co/X5SwPDQ/png-clipart-cartoon-stars-cartoon-star-removebg-preview.png"   width="115" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/7.mp3" controls >
                 </audio>
                 <h2>Star – estrella</h2>
-
+                </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-xl-3 p-2">
                 <br>
                 <img  class="mx-auto d-block"  src="https://i.ibb.co/N3d7WCP/02811416b8649deb6acb07722f826382-removebg-preview.png"    width="150"/>
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/8.mp3" controls >
                 </audio>
                 <h2>Trapezoid – trapezoide</h2>
+                </div>
             </div>
         </div>
     </div>
@@ -211,36 +211,48 @@
 
 
     <div class="container-fluid">
-        <div class="row mb-5">
-            <div class="col-md-3">
-
+        <div class="row ">
+            <div class="col-xl-3 p-2">
+<br>
                 <img class="mx-auto d-block"  src="https://i.ibb.co/cx7S72V/triangle-5258735-960-720-removebg-preview.png"  width="155" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/9.mp3" controls >
                 </audio>
                 <h2>Triangle – triángulo</h2>
+                </div>
             </div>
-            <div class="col-md-3">
-
+            <div class="col-xl-3 p-2">
+<br>
                 <img  class="mx-auto d-block"   src="https://i.ibb.co/v1K69nt/depositphotos-205275746-stock-illustration-two-finger-parallelogram-character-cartoon-removebg-previ.png"  width="192"/>
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/10.mp3" controls >
                 </audio>
                 <h2>Parallelogram - paralelogramo</h2>
             </div>
-            <div class="col-md-3">
+            </div>
+            <div class="col-xl-3 p-2">
 
-
+<br>
                 <img  class="mx-auto d-block" src="https://i.ibb.co/3BkL6Nz/2dce1ec378f622f7936e77a72cef76da-removebg-preview.png"  width="165" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/11.mp3" controls >
                 </audio>
                 <h2>Diamond – rombo</h2>
-
+                </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-xl-3 p-2">
                 <br><br>
-                <img  class="mx-auto d-block"  img src="https://i.ibb.co/189nK8x/png-transparent-oval-blog-oval-s-smiley-website-emoticon-removebg-preview.png"   width="175" />
+                <img  class="mx-auto d-block"  img src="https://i.ibb.co/189nK8x/png-transparent-oval-blog-oval-s-smiley-website-emoticon-removebg-preview.png"
+                      width="197" />
+                <br>
+                <div class="text-center">
                 <audio  id="player" src="AudioIngles/figura/12.mp3" controls >
                 </audio>
                 <h2>Oval - ovalo</h2>
+                </div>
             </div>
         </div>
     </div>
