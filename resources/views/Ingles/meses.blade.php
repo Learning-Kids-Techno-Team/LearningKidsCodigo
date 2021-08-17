19e49356643338eb2d329342f628df837ffd9087
LuzBonilla16 <luzalejandrabonilla@gmail.com>
2021-08-17 16:48:33 -0600

    Sprint 17 Luz Bonilla

--- resources/views/Ingles/meses.blade.php
+++ resources/views/Ingles/meses.blade.php
@@ -115,7 +115,7 @@
             <div class="col-md-12" style="background: #d7f3e3">
                 <div class="row ml-5 mr-5">
                     <div class="col-md-7  mt-5" style="background: #FDFD96">
-                        <img class="mt-5 mx-auto d-block"  src="https://i.ibb.co/x3rstQX/compu-removebg-preview.png" >
+                        <img class="mt-5 mx-auto d-block"  src="https://i.ibb.co/x3rstQX/compu-removebg-preview.png" width="450">
                     </div>
                     <div class="col-xl-5  mt-5 " style=" background: lightyellow">
                         <h4  class="mt-5" style="color: #3B5686">Inglés</h4>
